e10ef100a9b38d0f99a7599065a6b8bfdaac40d3
Aliyan Anwar <muhammad.aliyan22@gmail.com>
2023-11-28 02:21:21 +0500

    Create heikin_ashi.py

---
+++ heikin_ashi.py
@@ -0,0 +1,53 @@
+class heikin_ashi (adx_indicator):
+
+  def __init__(self, refine_list = None):
+
+      self.refine_list = refine_list
+
+  def run_heikin_ashi(self, refine_list ):
+
+      ha_status_list = self.get_heikin_ashi(self, bar_list )
+
+      return  ha_status_list
+
+  def heikin_ashi_status(self, ha_open , ha_close ):
+
+      candles =  np.full_like( ha_close, '', dtype='U10')
+
+      for i in range(1 , len(ha_close) ):
+
+            # green_condition =  ha_close[i] > ha_open[i]
+            # red_condition   =  ha_close[i] < ha_open[i]
+            if ha_close[i] > ha_open[i] :
+              candles[i]  = 'Green'
+
+            elif ha_close[i] < ha_open[i] :
+              candles[i]  = 'Red'
+
+            else:
+              candles[i] = 'Neutral'
+
+      return  candles
+
+
+  def heikin_ashi_candles (self, open, high, low, close ):
+
+      ha_close =  np.empty(len(close), dtype=np.float32 )
+
+      ha_high = np.empty(len(close), dtype=np.float32 )
+      ha_low  = np.empty(len(close), dtype=np.float32 )
+      ha_open = np.empty(len(close), dtype=np.float32 )
+
+      # candles =  np.empty(len(close) , dtype='U10')
+      # candles = [[]] *  len(close) , dtype=np.int16
+
+      ha_open[0]  = (open[0] + close[0] ) /2
+      ha_close[0] = (close[0] + open[0] + high[0] + low[0]) /4
+
+      for i in range(1 , len(close) ):
+            ha_open[i]  = (ha_open[i-1] + ha_close[i-1] ) / 2
+            ha_close[i] = (open[i] +  high[i] + low[i] + close[i]) / 4
+            ha_high[i]  = max( high[i], ha_open[i], ha_close[i]  )
+            ha_low[i]   = min( low[i], ha_open[i], ha_close[i]  )
+
+      return   ha_open, ha_close, ha_high, ha_low
